425da0bb0d1d1fdc5ee243a90956e2c607bd612a
Roald Ong <roald.ong@gmail.com>
2023-01-04 16:36:57 +0800

    Add in bootstrap css

--- app/javascript/bundles/MainMenu/components/MainMenu.jsx
+++ app/javascript/bundles/MainMenu/components/MainMenu.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import style from './MainMenu.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MainMenu = (props) => {
   const [name, setName] = useState(props.name);
@@ -8,6 +9,10 @@ const MainMenu = (props) => {
   return (
     <div>
       <h3>this is a test</h3>
+      <p>
+      <a href="#" class="btn btn-primary my-2">Main call to action</a>
+      <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+    </p>
     </div>
   );
 };
