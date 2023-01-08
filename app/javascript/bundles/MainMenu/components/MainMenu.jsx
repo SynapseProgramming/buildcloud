9ee78337cebf0a42103646d036b8d1da9a923c4f
Roald Ong <roald.ong@gmail.com>
2023-01-08 16:26:18 +0800

    Add in timeseries chart example

--- app/javascript/bundles/MainMenu/components/MainMenu.jsx
+++ app/javascript/bundles/MainMenu/components/MainMenu.jsx
@@ -3,16 +3,46 @@ import React, { useState } from 'react';
 import style from './MainMenu.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { TimeSeries, TimeRange} from "pondjs";
+import {
+    Charts,
+    ChartContainer,
+    ChartRow,
+    YAxis,
+    LineChart
+} from "react-timeseries-charts";
+
+const data = {
+
+name: "test1",
+columns: ["time","value"],
+points: [[12345,1],[33333,3]]
+
+};
+
+const series1 = new TimeSeries(data);
+
+
 const MainMenu = (props) => {
   const [name, setName] = useState(props.name);
 
+
+
   return (
     <div>
-      <h3>this is a test</h3>
+      {/* <h3>this is a test</h3>
       <p>
-      <a href="#" class="btn btn-primary my-2">Main call to action</a>
-      <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-    </p>
+      <a href="#" class="btn btn-primary my-1">Main call to action</a>
+      <a href="#" class="btn btn-secondary my-1">Secondary action</a>
+    </p> */}
+    <ChartContainer timeRange={series1.timerange()} width={800}>
+    <ChartRow height="200">
+        <YAxis id="axis1" label="AUD" min={0} max={5} width="60" type="linear" format="$,.2f"/>
+        <Charts>
+            <LineChart axis="axis1" series={series1} column={["aud"]}/>
+        </Charts>
+    </ChartRow>
+</ChartContainer>
     </div>
   );
 };
