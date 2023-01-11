fd40e7a28ffec56c437fee504bc648a5ca2caad8
Roald Ong <roald.ong@gmail.com>
2023-01-11 17:25:19 +0800

    First working graph with data

--- app/javascript/bundles/MainMenu/components/MainMenu.jsx
+++ app/javascript/bundles/MainMenu/components/MainMenu.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import style from "./MainMenu.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -21,10 +21,22 @@ const data = {
   ],
 };
 
+const sampleSeries = {
+  name: "test1",
+  columns: ["time", "value"],
+  points: [],
+};
+
+let seriesX = JSON.parse(JSON.stringify(sampleSeries));
+
 const series1 = new TimeSeries(data);
 
 const MainMenu = (props) => {
   const [name, setName] = useState(props.name);
+
+  const [mainX, setmainX] = useState(new TimeSeries(data));
+
+  const [isLoading, setLoading] = useState(true);
 
   // const [incomingData, setin]
 
@@ -40,15 +52,39 @@ const MainMenu = (props) => {
           throw new Error("Network response was not ok.");
         })
         .then((response) => {
-          // setremainingTasks(response.task_count);
-          console.log(response);
+          console.log("ran here!");
+          // print out incoming data
+          //TODO: create a new series for each x,y,z value
+          response.forEach((element) => {
+            // console.log("X value" + element.X);
+            // console.log("Y value" + element.Y);
+            seriesX["points"].push([element.Time, element.X]);
+          });
+          // console.log(seriesX);
+          if (isLoading) {
+
+          console.log(seriesX);
+            setmainX(new TimeSeries(seriesX));
+
+          console.log("running");
+            setLoading(false);
+          }
+          // MAIN = new TimeSeries(seriesX);
         })
         .catch(() => console.log("something went wrong!"));
     }
     return () => {
+      // setmainX(new TimeSeries(data));
+
+      console.log("finished");
+
       is_mounted = false;
     };
-  });
+  },[]);
+
+  if (isLoading) {
+    return <p>Loading!</p>;
+  }
 
   return (
     <div>
@@ -58,19 +94,19 @@ const MainMenu = (props) => {
       <a href="#" class="btn btn-secondary my-1">Secondary action</a>
     </p> */}
 
-      <ChartContainer timeRange={series1.timerange()} width={800}>
+      <ChartContainer timeRange={mainX.timerange()} width={800}>
         <ChartRow height="200">
           <YAxis
             id="axis1"
             label="AUD"
-            min={0}
-            max={5}
+            min={-1}
+            max={7}
             width="60"
             type="linear"
-            format="$,.2f"
+            format=",.2f"
           />
           <Charts>
-            <LineChart axis="axis1" series={series1} column={["aud"]} />
+            <LineChart axis="axis1" series={mainX} column={["aud"]} />
           </Charts>
         </ChartRow>
       </ChartContainer>
